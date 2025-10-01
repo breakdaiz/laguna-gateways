9eeee5fd6e8492240de019efb7f109143ede58e4
bernardrayoso <rayosobernard@gmail.com>
2025-10-02 03:40:59 +0800

    Add Radix UI components and enhance global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 function HomePage() {
-  return <div className='p-5'>Home</div>;
+  return (
+    <div className='p-5 flex flex-col w-max gap-5'>
+      <h1>HomePage</h1>
+      <Button>Default Button</Button>
+      <Button variant='outline'>Outline Button</Button>
+      <Input placeholder='First Name' />
+    </div>
+  );
 }
 
 export default HomePage;
